acfa4e390132fc1550dc89aa05385ad5532c6de1
miguelms19 <miguelms19@gmail.com>
2018-03-22 00:07:28 +0000

    amended settings

--- livewire/settings.py
+++ livewire/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'livewire/static/')
+    os.path.join(BASE_DIR.child('static'),)
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
@@ -136,15 +136,3 @@ MEDIA_URL = '/media/'
 # Configure Django App for Heroku.
 import django_heroku
 django_heroku.settings(locals())
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
