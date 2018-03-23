9226c1a5bca35c8c6028723c29a30e608a5eb398
miguelms19 <miguelms19@gmail.com>
2018-03-23 20:00:02 +0000

    work template finished

--- work/views.py
+++ work/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
 
-from .models import Job
+from jobs.models import Job
 # Create your views here.
 
 def allwork(request):
     jobs = Job.objects
     return render(request, 'work/allwork.html', {'jobs':jobs})
-    
