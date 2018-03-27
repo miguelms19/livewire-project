f30db9d043224e963ead9675905dbd9aaa69ecfb
miguelms19 <miguelms19@gmail.com>
2018-03-27 16:42:54 +0100

    deleted ebextension

--- livewire/settings.py
+++ livewire/settings.py
@@ -144,6 +144,7 @@ MEDIA_URL = '/media/'
 
 # Configure Django App for Heroku.
 #import django_heroku
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
 
 #TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
