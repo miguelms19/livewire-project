d91e1b8de0cffd069aa2badae22b27d9c3cf0168
miguelms19 <miguelms19@gmail.com>
2018-03-27 16:22:14 +0100

    modified setting file

--- livewire/settings.py
+++ livewire/settings.py
@@ -144,6 +144,6 @@ MEDIA_URL = '/media/'
 
 # Configure Django App for Heroku.
 #import django_heroku
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 #TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
