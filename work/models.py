66dda4df9670d1e5a3f4315e930fcd5ffbffccb5
miguelms19 <miguelms19@gmail.com>
2018-03-29 00:05:24 +0100

    amended models

--- work/models.py
+++ work/models.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 # Create your models here.
 class Job(models.Model):
-    image = models.ImageField(upload_to='images/')
+    #image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=255, blank=True)
     summary = models.CharField(max_length=2000)
     web = models.CharField(max_length=2000, validators=[URLValidator()], blank=True)
