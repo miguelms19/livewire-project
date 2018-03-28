ce175852088eaf01fdb6c743aa5257dc5732ebca
Pedro Rodrigues <miguelms19@gmail.com>
2018-03-28 22:42:52 +0100

    Update models.py

--- jobs/models.py
+++ jobs/models.py
@@ -3,7 +3,7 @@ from django.core.validators import URLValidator
 
 # Create your models here.
 class Job(models.Model):
-    #image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='media/')
     title = models.CharField(max_length=255, blank=True)
     summary = models.CharField(max_length=2000)
     web = models.CharField(max_length=2000, validators=[URLValidator()], blank=True)
