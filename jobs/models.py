e6eead8818cdd50416f6303ad37f07899290b826
miguelms19 <miguelms19@gmail.com>
2018-03-28 23:56:30 +0100

    amended models

--- jobs/models.py
+++ jobs/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.core.validators import URLValidator
-from PIL import ImageField
+import PIL
+from PIL import Image
+
 
 # Create your models here.
 class Job(models.Model):
