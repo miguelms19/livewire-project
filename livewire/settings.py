3e685f83a5354cb322a6bdf134b43bdf5cac6e86
miguelms19 <miguelms19@gmail.com>
2018-03-29 17:28:23 +0100

    changed settings

--- livewire/settings.py
+++ livewire/settings.py
@@ -138,15 +138,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-#STATICFILES_DIRS = (
-#    os.path.join(BASE_DIR, 'static'),
-#)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-
-
-#STATICFILES_DIRS = [
-#    os.path.join(BASE_DIR, 'livewire/static/')
-#]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
