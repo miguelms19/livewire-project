cbf2ec8ff9d31e9fe36bb8f302baa9b341c48d99
miguelms19 <miguelms19@gmail.com>
2018-03-29 21:54:02 +0100

    updated settings

--- livewire/settings.py
+++ livewire/settings.py
@@ -156,8 +156,8 @@ STATICFILES_DIRS = [
 
 #STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/static/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
