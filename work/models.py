97b61d587058b653b9dee67f16c0cae46a990815
Pedro Rodrigues <miguelms19@gmail.com>
2018-03-29 02:56:30 +0100

    Update models.py

--- work/models.py
+++ work/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.core.validators import URLValidator
-import PIL
-from PIL import Image
 
 # Create your models here.
 class Job(models.Model):
-    #image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=255, blank=True)
     summary = models.CharField(max_length=2000)
     web = models.CharField(max_length=2000, validators=[URLValidator()], blank=True)
