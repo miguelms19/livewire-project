4ce1c08b78cfaf59a6e1f3edd23ba60c1a0ce7da
Pedro Rodrigues <miguelms19@gmail.com>
2018-03-29 02:55:53 +0100

    Update models.py

--- about/models.py
+++ about/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 from django.core.validators import URLValidator
-import PIL
-from PIL import Image
+
 
 # Create your models here.
 class Team(models.Model):
-    #image = models.ImageField(upload_to='images/')
+    image = models.ImageField(upload_to='images/')
     name = models.CharField(max_length=255, blank=True)
     summary = models.CharField(max_length=2000)
     web = models.CharField(max_length=2000, validators=[URLValidator()], blank=True)
