4c7b89997e977e389808bfec4aaec1dc3055d42f
miguelms19 <miguelms19@gmail.com>
2018-03-29 19:29:03 +0100

    updated settings

--- livewire/settings.py
+++ livewire/settings.py
@@ -144,9 +144,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'livewire/static/')
