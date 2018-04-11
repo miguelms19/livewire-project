c2723c10cac02391255cc026e80578efc31907f4
Lakshay Kumar <33530741+ineffablelucky@users.noreply.github.com>
2018-04-11 17:31:28 +0530

    added media2 MEDIA_ROOT

--- livewire/settings.py
+++ livewire/settings.py
@@ -157,8 +157,8 @@ STATICFILES_DIRS = [
 
 #STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media2/images/upload')
+MEDIA_URL = 'media2/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
