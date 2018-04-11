4d085cbb6f2e32fb31150fea821f9f6e9858a8a2
miguelms19 <miguelms19@gmail.com>
2018-04-11 22:27:07 +0100

    coded out airtable from views

--- work/views.py
+++ work/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
-from airtable import Airtable
+#from airtable import Airtable
 
 # Create your views here.
-AT = Airtable('app722UkuB0eVMwVk',
-              'Jobs',
-              api_key='keyoOFryShWQQ1qGs')
+#AT = Airtable('app722UkuB0eVMwVk',
+#              'Jobs',
+#              api_key='keyoOFryShWQQ1qGs')
 
 
 def allwork(request):
-    jobs = AT.get_all()
-    return render(request, 'work/allwork.html', {'jobs':jobs})
+#    jobs = AT.get_all()
+#    return render(request, 'work/allwork.html', {'jobs':jobs})
+    return render(request, 'work/allwork.html')
